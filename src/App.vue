c83c754f46e5fb1f147a948a5b76547a4ab99953
김선정 <seonj.kim@samsung.com>
2018-08-18 16:34:54 +0900

    update blog

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <div id="header">
+    </div>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -18,6 +20,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+/*
+#FAFAFA
+#CFCFCF  - #9E9E9E
+#6D6D6D
+#00AAFC
+#B2E4FC  - #015E92
+*/
+#header {
+  background-image: url('./assets/header2.jpg');
+  background-position: center;
+  width: 100%;
+  height: 400px;
+
 }
 </style>
