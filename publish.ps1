240c31d14afd498992081a0b44aceb644cd17732
김선정 <seonj.kim@samsung.com>
2018-08-18 16:37:02 +0900

    updqte publish script

--- publish.ps1
+++ publish.ps1
@@ -1,8 +1,8 @@
 cd ./dist
 git init
 git remote add origin https://github.com/lamoshalt811/lamoshalt811.github.io.git
-git pull
+git pull origin master
 git add .
-git commit -m 'update blog'
-git push
+git commit -m "update blog"
+git push -u -f origin master
 cd ..
