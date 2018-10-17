f4a650125f63cbb2cf5e9574ddc06468d2dd1b99
lamos <lamoshalt811@gmail.com>
2018-10-17 23:10:03 +0900

    create main page

---
+++ src/components/main/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <img src="../../assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Lamos blog'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './Main.scss'
+</style>
